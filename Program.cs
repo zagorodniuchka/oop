759d8ad147c00c3c1e75a61eeb9919ad0446c411
Anastasia <zagorodnyuk-03@bk.ru>
2023-09-12 22:51:31 +0300

    lab1#3

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Person {
+    public string Name {get; set;}
+    public int Age {get; set;}
+    public string Work {get; set;}
+    public bool IsMarried {get; set;}
+	
+	public Person(string name, int age, string work, bool isMarried) {
+        Name = name;
+        Age = age;
+        Work = work;
+        IsMarried = isMarried;
+    }
+
+    public void Hello(){
+        Console.WriteLine($"Hello! My name is {Name}. I am {Age} years old. I work at {Work}.");
+    }
+    public void Marriage(){
+        if (IsMarried){
+            System.Console.WriteLine("I am married");
+        }else{
+            System.Console.WriteLine("I am not married");
+        }
+    }
+}
+
+public class Program {
+    public static void Main() {
+        Person person = new Person("Anastasia", 19, "Galex Studio", false);
+        person.Hello();
+        person.Marriage();
+    }
+}
