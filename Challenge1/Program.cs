6e59d8ecb4974545337b9c263ead5d2961c28f8c
Anastasia <zagorodnyuk-03@bk.ru>
2023-09-19 13:54:06 +0300

    Challenge1 and fixed the srtucture of the project

---
+++ Challenge1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public partial class Program{  
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public User(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public string GetInitials()
+        {
+            return $"{FirstName} {LastName} -> {FirstName[0]}. {LastName[0]}.";
+        }
+    }
+
+    public static void Main()
+    {
+        User user = new User("Anastasia", "Zagorodniuc");
+        string initials = user.GetInitials();
+        Console.WriteLine(initials);
+    }
+}
