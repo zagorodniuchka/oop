8c503a728a721da4e77ecfa1d37418028598d05b
Anastasia <zagorodnyuk-03@bk.ru>
2023-11-28 00:26:03 +0200

    creating method for print info about all files

---
+++ Lab3/FilesInfo/PrintInfoAllFiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Lab2
+{
+    public class InfoAllFiles
+    {
+        protected string filePath = @"TestOOP/";
+
+        public void PrintInfoFiles()
+        {
+            string[] files = Directory.GetFiles(filePath);
+            foreach (var item in files)
+            {
+                FileInfo fileInfo = new FileInfo(item);
+                Console.WriteLine("File Name: " + fileInfo.Name);
+                Console.WriteLine("Extension: " + fileInfo.Extension);
+                Console.WriteLine("Created: " + fileInfo.CreationTime);
+                Console.WriteLine("Last Updated: " + fileInfo.LastWriteTime);
+            }
+        }
+        public void PrintInfoSpecificFile(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(Path.Combine(filePath, fileName));
+            Console.WriteLine("File Name: " + fileInfo.Name);
+            Console.WriteLine("Extension: " + fileInfo.Extension);
+        }
+    }
+}
