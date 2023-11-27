f3ef5ca857ee74bc2185d124b5ade5286f6f524e
Anastasia <zagorodnyuk-03@bk.ru>
2023-11-28 01:46:35 +0200

    creating method that ypfates data for the snapshot

---
+++ Lab3/CommitAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Lab2
+{
+    public class CommitAction
+    {
+        protected  DateTime snapshotTime = DateTime.Now;
+       
+		public DateTime UpdateSnaphotTime() 
+        {
+			DateTime snapshotTime = DateTime.Now;
+            Console.WriteLine("Snapshot time updated to: " + snapshotTime);
+            return snapshotTime;
+        }
+    }
+}
---
+++ Lab3/FileSnaphot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Lab2
+{
+    public class FileSnapshot
+    {
+        private DateTime LastModified { get; set; }
+
+		public static implicit operator FileSnapshot(DateTime v)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
