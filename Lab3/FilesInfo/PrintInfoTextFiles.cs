1db5472261d199c7ffbf6e20ced97064c4a79480
Anastasia <zagorodnyuk-03@bk.ru>
2023-11-28 00:56:44 +0200

    method for  printing info  for text file

---
+++ Lab3/FilesInfo/PrintInfoTextFiles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Lab2
+{
+	public class InfoTextFiles : InfoAllFiles
+	{
+		
+		private int lines = 0, words=0, characters=0;
+        
+        public void GetCountLines(string fileName) 
+		{
+			lines= File.ReadLines(Path.Combine(filePath, fileName)).Count();
+        }
+		public void GetCountWords(string fileName)
+		{
+            string[] lines = File.ReadAllLines(Path.Combine(filePath, fileName));
+            words = lines.SelectMany(line => line.Split(new[] { ' ','.','?','!','/' }, StringSplitOptions.RemoveEmptyEntries)).Count();
+
+        }
+		public void GetCountCharacter(string fileName)
+		{
+            string[] lines = File.ReadAllLines(Path.Combine(filePath, fileName));
+            characters = lines.Sum(line => line.Length);
+        }
+        public void PrintFileInfo(string fileName) 
+		{
+            PrintInfoSpecificFile(Path.Combine(filePath, fileName));
+            Console.WriteLine($"Number of lines: {lines}");
+			Console.WriteLine($"Number of words: {words}");
+			Console.WriteLine($"Number of characters: {characters}");
+		}
+	}
+}
