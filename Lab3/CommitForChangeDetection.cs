860159ed2279dfe2a94cd7b70d367b407455029a
Anastasia <zagorodnyuk-03@bk.ru>
2023-11-28 02:00:50 +0200

    snapshot storage file manager

---
+++ Lab3/CommitForChangeDetection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab2
+{
+	public class CommitForChangeDetection : CommitAction
+	{
+		// overriding
+		new public DateTime UpdateSnaphotTime()
+		{
+			DateTime snapshotTime = DateTime.Now;
+			return snapshotTime;
+		}
+	}
+}
---
+++ Lab3/SnapshotStorageFileManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab2
+{
+	public class SnapshotStorageFileManager
+	{
+		private string fileName = "Snapshots.txt";
+		private string filePath = @"TestOOP";
+
+		public void StoreSnapshot(DateTime snapshotTime) 
+		{
+			string FMT = "O";
+			string contentToSave = snapshotTime.ToString(FMT)+"\n";
+			string[] files = Directory.GetFiles(filePath);
+			foreach (var item in files) 
+			{
+				FileInfo fileInfo = new FileInfo(item);
+				contentToSave += fileInfo.Name + "\n";	
+			}
+			File.WriteAllText(fileName, contentToSave);
+		}
+		public void ExtractLastSnapshot(DateTime snapshotTime)
+		{
+			string FMT = "O";
+			string[] contentToExtract = File.ReadAllLines(fileName);
+			snapshotTime = DateTime.ParseExact(contentToExtract[0], FMT, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
+			Console.WriteLine("Created snapshot at: " + snapshotTime);
+		}
+		public void ExtractSavedFiles(List<string> savedFiles) 
+		{
+			string[] contentToExtract = File.ReadAllLines(fileName);
+			for (int i = 1; i < contentToExtract.Length; i++)
+			{
+				savedFiles.Add(contentToExtract[i]);
+			}
+		}
+	}
+}
